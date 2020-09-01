422b14ad4a731c68db1656e1bca619d05d27a083
Sam Panza <sam@panza>
2020-09-02 00:50:51 +0700

    #2 Here we go with tests

---
+++ kluster/src/test/java/su/ptx/kfkadmin/kluster/KlusterTests.java
@@ -0,0 +1,34 @@
+package su.ptx.kfkadmin.kluster;
+
+import java.util.Map;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.Admin;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@EmbeddedKafka(count = 2, partitions = 3, topics = {"foo", "bar"})
+class KlusterTests {
+  private Kluster kluster;
+
+  @BeforeEach
+  void setUp() {
+    kluster = new Kluster(
+        Admin.create(
+            Map.of(
+                CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG,
+                System.getProperty(EmbeddedKafkaBroker.SPRING_EMBEDDED_KAFKA_BROKERS))));
+  }
+
+  @AfterEach
+  void tearDown() {
+    kluster.close();
+  }
+
+  @Test
+  void ok() {
+    //
+  }
+}
