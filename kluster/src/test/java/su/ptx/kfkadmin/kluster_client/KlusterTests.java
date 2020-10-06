48f37bd09b1d905b4300571ce3c5b9c8abace56e
Sam Panza <sam@panza>
2020-10-06 17:59:10 +0700

    #3 clearTopik test OK

--- kluster/src/test/java/su/ptx/kfkadmin/kluster_client/KlusterTests.java
+++ kluster/src/test/java/su/ptx/kfkadmin/kluster_client/KlusterTests.java
@@ -1,11 +1,13 @@
 package su.ptx.kfkadmin.kluster_client;
 
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import su.ptx.kfkadmin.kluster.Kluster;
 
 import java.util.Map;
@@ -13,18 +15,24 @@ import java.util.Map;
 @EmbeddedKafka(count = 2, partitions = 3, topics = {"foo", "bar"})
 abstract class KlusterTests {
   Kluster kluster;
+  Producer<Integer, String> intStrProducer;
 
   @BeforeEach
-  void setUp() {
+  void setUp(EmbeddedKafkaBroker broker) {
     kluster = new Kluster(
       Admin.create(
         Map.of(
-          CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG,
-          System.getProperty(EmbeddedKafkaBroker.SPRING_EMBEDDED_KAFKA_BROKERS))));
+          "bootstrap.servers",
+          broker.getBrokersAsString())));
+
+    var configs = KafkaTestUtils.producerProps(broker);
+    configs.put("acks", "all");
+    intStrProducer = new KafkaProducer<>(configs);
   }
 
   @AfterEach
   void tearDown() {
     kluster.close();
+    intStrProducer.close();
   }
 }
--- kluster/src/test/java/su/ptx/kfkadmin/kluster_client/RecordsTests.java
+++ kluster/src/test/java/su/ptx/kfkadmin/kluster_client/RecordsTests.java
@@ -1,11 +1,7 @@
 package su.ptx.kfkadmin.kluster_client;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Test;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
-import su.ptx.GetFuture;
 
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -14,19 +10,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class RecordsTests extends KlusterTests {
   @Test
-  void clearTopik(EmbeddedKafkaBroker broker) {
-    int msgNum = 100;
-    try (var producer = new KafkaProducer<Integer, String>(KafkaTestUtils.producerProps(broker))) {
-      IntStream.range(0, msgNum)
-        .mapToObj(number -> new ProducerRecord<>("foo", number, String.valueOf(number)))
-        .map(producer::send)
-        .forEach(rmdf -> new GetFuture<>().apply(rmdf));
-    }
-    var foo = kluster.topik("foo");
-    assertEquals(msgNum, foo.size());
-    var lowWatermarks = foo.clear();
-    assertEquals(0, foo.size());
+  void clearTopik() {
+    var N = 100;
+    IntStream.range(0, N)
+      .mapToObj(i -> new ProducerRecord<>("quuz", i, String.valueOf(i)))
+      .forEach(intStrProducer::send);
+    intStrProducer.flush();
+    var quuz = kluster.topik("quuz");
+    assertEquals(N, quuz.size());
+    var lowWatermarks = quuz.clear();
+    assertEquals(0, quuz.size());
     assertEquals(3, lowWatermarks.length);
-    assertEquals(msgNum, LongStream.of(lowWatermarks).sum());
+    assertEquals(N, LongStream.of(lowWatermarks).sum());
+    quuz.delete();
   }
 }
